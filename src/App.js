6981b81146cd9c6f982aeb5871faab14d12a26a4
Sydney Holland <sydneymh@udel.edu>
2025-08-13 15:42:20 -0400

    Added initial pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Games from './pages/Games';
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav style={{ padding: "1rem", backgroundColor: "#282c34" }}>
+        <Link to ="/" style= {{ color: "white", marginRight: "1rem" }}> Home </Link>
+        <Link to="/about" style= {{ color: "white" }} > About </Link>
+        <Link to="/games" style= {{ color: "white", marginLeft: "1rem" }} > Games </Link>
+      </nav>
+          <Routes>
+            <Route path="/" element={<Home /> } />
+            <Route path="/about" element={<About />} />
+            <Route path="/games" element={<Games />} />
+          </Routes>
     </div>
+    </Router>
   );
 }
 
